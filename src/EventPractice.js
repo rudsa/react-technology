9adba283e65abd852e2182aa7370b04bd289a964
zmainsk <skkim@j2stechno.com>
2023-05-08 18:09:13 +0900

    react EventPractice Function

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,44 +1,42 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class EventPractice extends Component {
-  state = {
-    message: "",
+const EventPractice = () => {
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+
+  const onChangeUsername = (e) => setUsername(e.target.value);
+  const onChangeMessage = (e) => setMessage(e.target.value);
+
+  const onClick = () => {
+    alert(username + " " + message);
+    setUsername("");
+    setMessage("");
+  };
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onClick();
+    }
   };
 
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({
-      message: e.target.value,
-    });
-  }
-
-  handleClick(e) {
-    alert(this.state.message);
-    this.setState({
-      message: "",
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Practice</h1>
-        <input
-          type="text"
-          name="message"
-          placeholder="Test"
-          value={this.state.message}
-          onChange={this.handleChange}
-        />
-        <button onClick={this.handleClick}>Check</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>Event Practice</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="User Name"
+        onChange={onChangeUsername}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="Please Enter Message"
+        onChange={onChangeMessage}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={onClick}>Check</button>
+    </div>
+  );
+};
 
 export default EventPractice;
