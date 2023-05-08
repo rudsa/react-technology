2eaad96d45d1fe2cd3d4ddace8512b824f63e17d
zmainsk <skkim@j2stechno.com>
2023-05-08 16:57:11 +0900

    react eventHandler

---
+++ src/EventPractice.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class EventPractice extends Component {
+  state = {
+    message: "",
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      message: e.target.value,
+    });
+  }
+
+  handleClick(e) {
+    alert(this.state.message);
+    this.setState({
+      message: "",
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Practice</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="Test"
+          value={this.state.message}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleClick}>Check</button>
+      </div>
+    );
+  }
+}
+
+export default EventPractice;
