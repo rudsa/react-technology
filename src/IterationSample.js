67be1a38f72b355180e40bd786622c7e765a3d15
rudsa <rudsa@naver.com>
2023-05-09 22:11:44 +0900

    react array & key

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -10,9 +10,36 @@ const IterationSample = () => {
   const [inputText, setInputText] = useState("");
   const [nextId, setNextId] = useState(5);
 
-  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  const onChange = (e) => setInputText(e.target.value);
 
-  return <ul>{nameList}</ul>;
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText("");
+  };
+
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+
+  const nameList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+
+  return (
+    <fragment>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>Add Name</button>
+      <ul>{nameList}</ul>
+    </fragment>
+  );
 };
 
 export default IterationSample;
